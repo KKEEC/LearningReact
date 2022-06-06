4cbe5d300c1915fbc77f8803c346c517ed6398aa
Krishna <krishnakeec@gmail.com>
2022-06-06 12:19:27 +0300

    Higher order function

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import './App.css';
+
+const Button = (props) => {
+  return(
+    <button onClick={props.handleClick}>{props.text}</button>
+  )
+}
+
+const Display = (props) => <div>{props.value}</div>
+
+const App = () => {
+  const [value, setValue] = useState(10)
+
+  const setToValue = (newValue) => {
+    console.log('Value now', newValue)
+    setValue(newValue)
+  } 
+
+  
+
+  
+
+  return (
+    <div>
+      <Display value={value}/>
+      <Button handleClick={() => setToValue(1000) } text='thousand' />
+      <Button handleClick={() => setToValue(0) } text='reset' />
+      <Button handleClick={() => setToValue(value + 1) } text='increment' />
+      
+    </div>
+  )
+}
+
+export default App;
